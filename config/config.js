09910443c21f61444528891032b19639b5afbb05
anika308142 <azrin.nahr308@gmail.com>
2022-04-11 00:13:18 +0600

    modifications

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "edakpeon_dev",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
